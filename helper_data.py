98b897748c9b5ae7bb8049168d3a19d929cb2cc9
Jabberdrake <jabberdrake@gmail.com>
2024-11-02 12:54:17 +0100

    buncha stuff (still fucking crashes tho)

--- helper_data.py
+++ helper_data.py
@@ -4,7 +4,7 @@
 ALMANAC_NAME = "Almanac of Galitas"
 ALMANAC_ID = "1SjlQj7fh65_2u0yAp32Sc-J8injhJ0zUvlC27soAzSs"
 PROVS_SHEET = "Provinces"
-FIRST_TEMP_WASTELAND = 4971
+FIRST_TEMP_WASTELAND = 4956
 MAX_PROVINCES = 5000
 
 ### Simple maps used for non-trivial name assignment
